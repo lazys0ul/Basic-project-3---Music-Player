958aab1721889e7e6b37d6b1cc9a3c7810071ae5
Khatta Karela <pranavpriyadarshi903@gmail.com>
2025-07-25 16:50:48 +0530

    feat: add static file serving for uploads directory

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './config/mongoDB.js'
 import adminRouter from './routes/adminRoutes.js'
+import path from 'path'
 
 dotenv.config()
 connectDB()
@@ -12,6 +13,8 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors())
+
+app.use('/upload', express.static(path.join(path.resolve(), 'uploads')))
 
 app.use('/api/admin', adminRouter)
 
